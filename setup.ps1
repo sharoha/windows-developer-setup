565b9c588caed0b4cc6eff7d9a7b79dde4dab95c
Rohan Sharma <sharmarohan136@gmail.com>
2021-03-03 02:02:48 +0530

    Added comments on how to use, and added command to install vscode as well

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,5 @@
+# copy the command in line 2 in Powershell and run it before executing this script
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 function Check_Scoop_Not_installed($scoop) {
     $path = (Get-ChildItem Env:Path).Value.Split(";");
     foreach ($item in $path) {
@@ -11,18 +13,23 @@ function Check_Scoop_Not_installed($scoop) {
 $isScoopNotInstalled = Check_Scoop_Not_installed('scoop\shims');
 Write-Output $isScoopNotInstalled;
 if ($isScoopNotInstalled) {
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')    
 }
 
 scoop install git
+
+scoop bucket add java
+scoop bucket add extras
+scoop update
+
 scoop install 7zip
+scoop install winrar
 scoop install grep
 scoop install concfg
 scoop install pshazz
 scoop install touch
 scoop install vim
-scoop bucket add java
 scoop install openjdk11
 scoop install gradle
+scoop install vscode
 
